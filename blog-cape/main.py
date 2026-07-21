d143a1854c7caa11a985fe8d49d3ad0acc36493a
Your Name <prajwal.rao@xoriant.com>
2026-07-21 09:45:57 +0000

    Capestone 57

--- blog-cape/main.py
+++ blog-cape/main.py
@@ -1,11 +1,19 @@
 from flask import Flask, render_template
+from post import Post
 
-
+post = Post()
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    posts = post.get_posts() 
+    return render_template("index.html", posts = posts)
+
+@app.route("/<int:id>")
+def get_post(id):
+    post_info = post.get_posts_by_id(id)
+    print(post_info)
+    return render_template("post.html", post=post_info)
 
 if __name__ == "__main__":
     app.run(debug=True)
