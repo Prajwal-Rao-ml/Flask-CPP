4df891f096f26f4d665088b3c0445682c92ddfec
Your Name <prajwal.rao@xoriant.com>
2026-07-21 09:45:57 +0000

    Capestone 57

--- blog-cape/post.py
+++ blog-cape/post.py
@@ -1,2 +1,9 @@
+import requests
 class Post:
-    pass
+    def get_posts(self):
+        # Logic to retrieve posts from the database or any data source
+        posts = requests.get("https://api.npoint.io/85d8e612305a07f7b20c")
+        return posts.json()
+    def get_posts_by_id(self,id):
+        post = requests.get("https://api.npoint.io/85d8e612305a07f7b20c/{}".format(id))
+        return post.json()
