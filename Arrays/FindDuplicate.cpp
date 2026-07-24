2bed60c3ba948ee8bed7766c70d2edd63ca85eaf
Your Name <prajwal.rao@xoriant.com>
2026-07-24 09:26:15 +0000

    Find duplicate and find unique occurances

---
+++ Arrays/FindDuplicate.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<unordered_map>
+using namespace std;
+
+class Duplictes{
+    private:
+    unordered_map<int,int> map;
+    public:
+    void findDuplicate(int array[], int size){
+        for(int i=0;i<size;i++){
+            map[array[i]]++;
+        }
+
+        for(auto& [key,value]:map){
+            if(value>1){
+                cout<<"The duplicate element is "<<key<<endl;
+                return;
+            }
+        }
+    }
+};
+
+int main(){
+    int array[]={1,2,3,4,5,6,7,8,9,10,1};
+    int size=sizeof(array)/sizeof(array[0]);
+    Duplictes obj;
+    obj.findDuplicate(array,size);
+    return 0;
+}
