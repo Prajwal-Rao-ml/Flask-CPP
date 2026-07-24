2bc710f1ebc73e9d3d1a9859c6622552fe2542ac
Your Name <prajwal.rao@xoriant.com>
2026-07-24 06:21:28 +0000

    reverese array

---
+++ Arrays/ReverseArray.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+class ReverseArray{
+    public:
+    void reverseArray(int array[], int length){
+        int i = 0, j = length-1;
+        
+        while(i<=j){
+            int temp = array[j];
+            array[j--] = array [i];
+            array[i++] = temp;
+        }
+    }
+};
+
+int main(){
+    int array[5]={1,2,3,4,5};
+    ReverseArray arr;
+    cout<<"before reversing"<<endl;
+    for(int i=0;i<5;i++){
+        cout<< array[i] << " ";
+    }
+    cout<<endl;
+    arr.reverseArray(array,5);
+    cout<<"After reversing"<<endl;
+    for(int i=0;i<5;i++){
+        cout<< array[i] << " ";
+    }
+    cout<<endl;
+    return 0;
+}
